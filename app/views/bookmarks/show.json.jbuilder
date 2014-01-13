14a9e5d3fc3d6c581b735519ae3bd887413bb640
kishore-mohan <kishorevlrtn@gmail.com>
2014-01-13 22:24:38 +0530

    initial commit

---
+++ app/views/bookmarks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @bookmark, :title, :url, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookmarkApplication::Application.config.secret_key_base = '9bda609d0a1c46bea2b22447215a698d36a6578a7be36dd48af6cf996822b48801ed196001b57b84449db615570ece2b6b8716127b05a9a5dff695f41a9adb83'
